674e29a2c20b882d2d52a00c80a2537565c2b445
zrydys-mgmt <zrydys@gmail>
2025-01-25 14:40:48 +0100

    gallery js + css

---
+++ css/gal.js
@@ -0,0 +1,27 @@
+console.log('gal st');
+
+    // Simple navigation function
+    function goTo(page) {
+        window.location.href = page; // Navigate to the selected page
+    }
+    
+    //scroll arrows
+    const leftBtn = document.querySelector('.left-btn');
+const rightBtn = document.querySelector('.right-btn');
+const gallery = document.querySelector('.gallery');
+
+leftBtn.addEventListener('click', () => {console.log('gal click');
+  gallery.scrollBy({
+    left: -150, // Scrolls 150px to the left when the left button is clicked
+    behavior: 'smooth'
+  });
+});
+
+rightBtn.addEventListener('click', () => {
+  gallery.scrollBy({
+    left: 150, // Scrolls 150px to the right when the right button is clicked
+    behavior: 'smooth'
+  });
+});
+
+console.log('gal OK');
