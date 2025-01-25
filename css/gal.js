170a8f876f1137b8a2ed0eb34aed10c3d88c9606
zrydys-mgmt <zrydys@gmail>
2025-01-25 19:08:59 +0100

    refactored& moved 2024R&D into prevRnD; replaced by modern Gallery

--- css/gal.js
+++ css/gal.js
@@ -10,16 +10,16 @@ console.log('gal st');
 const rightBtn = document.querySelector('.right-btn');
 const gallery = document.querySelector('.gallery');
 
-leftBtn.addEventListener('click', () => {console.log('gal click');
-  gallery.scrollBy({
-    left: -150, // Scrolls 150px to the left when the left button is clicked
-    behavior: 'smooth'
-  });
-});
-
 rightBtn.addEventListener('click', () => {
   gallery.scrollBy({
     left: 150, // Scrolls 150px to the right when the right button is clicked
+    behavior: 'smooth'
+  });console.log('gal click rg');
+});
+
+leftBtn.addEventListener('click', () => { console.log('gal click');
+  gallery.scrollBy({
+    left: -150, // Scrolls 150px to the left when the left button is clicked
     behavior: 'smooth'
   });
 });
