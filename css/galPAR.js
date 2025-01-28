434065715d178fe375af6652a533a3914427223e
zrydys-mgmt <zrydys@gmail>
2025-01-28 20:30:33 +0100

    fix vert-scroll-hidden +add gallery frame

---
+++ css/galPAR.js
@@ -0,0 +1,28 @@
+console.log('gal st parent.');
+
+    // Simple navigation function <a href="#" target="_parent">Link Text</a>
+    function goTo(page) {
+        //window.location.href = page; // Navigate to the selected page
+        window.parent.location.href = page; // Navigate to the selected page
+    }
+    
+    //scroll arrows
+    const leftBtn = document.querySelector('.left-btn');
+const rightBtn = document.querySelector('.right-btn');
+const gallery = document.querySelector('.gallery');
+
+rightBtn.addEventListener('click', () => {
+  gallery.scrollBy({
+    left: 150, // Scrolls 150px to the right when the right button is clicked
+    behavior: 'smooth'
+  });console.log('gal click rg');
+});
+
+leftBtn.addEventListener('click', () => { console.log('gal click');
+  gallery.scrollBy({
+    left: -150, // Scrolls 150px to the left when the left button is clicked
+    behavior: 'smooth'
+  });
+});
+
+console.log('gal OK parent. ');
